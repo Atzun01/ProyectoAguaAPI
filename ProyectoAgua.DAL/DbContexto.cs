d7620444d7e688403f2a36c4841aa7786123cbe1
Atzun01 <Emoralesatzun@outlook.com>
2024-09-20 15:36:11 -0600

    Paquetes

--- ProyectoAgua.DAL/DbContexto.cs
+++ ProyectoAgua.DAL/DbContexto.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
 
 namespace ProyectoAgua.DAL
 {
-    internal class DbContexto
+    internal class DBContexto:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=hotplaterestaurant;User=root;Password=password;",
+                new MySqlServerVersion(new Version(8, 0, 0)));
+        }
+    
     }
 }
