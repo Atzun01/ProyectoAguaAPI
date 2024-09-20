b9a9ec29257e9133904a0f9f0c3f377a3910e63b
Atzun01 <Emoralesatzun@outlook.com>
2024-09-20 15:49:21 -0600

    Error 404

--- ProyectoAgua.DAL/DbContexto.cs
+++ ProyectoAgua.DAL/DbContexto.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ProyectoAgua.EN;
 
 
 
@@ -17,6 +18,17 @@ namespace ProyectoAgua.DAL
             optionsBuilder.UseMySql("Server=localhost;Database=hotplaterestaurant;User=root;Password=password;",
                 new MySqlServerVersion(new Version(8, 0, 0)));
         }
-    
+        /*
+        public DbSet<Consumo> Consumos { get; set; }
+        public DbSet<DerechoAgua> DerechoAguas { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Mecha> Mechas { get; set; }
+        public DbSet<Mora> Moras { get; set; }
+        public DbSet<RegistroAgua> RegistroAguas { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        */
+
     }
+
 }
