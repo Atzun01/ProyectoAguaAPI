119bbbefa936fd9f2e10a2cd64437ebfcf4a4b9d
Atzun01 <Emoralesatzun@outlook.com>
2024-09-23 13:46:25 -0600

    Cree los Controladores API

---
+++ ProyectoAguaAPI/Controller/ConsumoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAguaAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumoController : ControllerBase
+    {
+    }
+}
