35ecac33c411730b5f48d38a13aedf92c9ba5545
Atzun01 <Emoralesatzun@outlook.com>
2024-10-09 17:11:01 -0600

    PruebasUnitarias de Usuario

---
+++ ProyectoAguaPruebaUnitarias/RolBLTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoAgua.BL;
+using ProyectoAgua.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ProyectoAgua.BL.Tests
+//{
+//    [TestClass()]
+//    public class RolBLTests
+//    {
+//        private static Rol rolinicial = new Rol { Id = 24 }; // Rol existente
+//        private RolBL rolBL = new RolBL();
+
+//        public async Task T1CrearAsyncTest()
+//        {
+//            var rol = new Rol();
+//            rol.Nombre = "Admin 3";
+//            int result = await rolBL.CrearRolAsync(rol);
+//            Assert.AreNotEqual(0, result);
+//        }
+
+//        [TestMethod()]
+//        public async Task T2ModificarRolAsyncTest()
+//        {
+//            var rol = new Rol();
+//            rol.Id = rolinicial.Id;
+//            rol.Nombre = "Admin33";
+//            int result = await rolBL.ModificarRolAsync(rol);
+//            Assert.AreEqual(1, result);
+
+//        }
+
+//        [TestMethod()]
+//        //public void EliminarRolAsyncTest()
+//        //{
+        //    var rol = Rol();
+        //    rol.Id = rolInicil.Id
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public async void ObtenerRolPorIdAsyncTest()
+        //{
+        //    var rol = Rol();
+        //    rol.Id = rolinicial.Id;
+        //    var result = await rolBL.ModificarAsync(rol);
+        //    Assert.AreEqual(rol.Id, result.Id);
+        //}
+
+        //[TestMethod()]
+        //public void ObtenerTodosRolesAsyncTest()
+        //{
+        //    var result =
+        //    Assert.Fail();
+        //}
+
+    //    //[TestMethod()]
+    //    //public void BuscarRolesAsyncTest()
+    //    //{
+    //    //    Assert.Fail();
+    //    //}
+    //}
+
+//}
---
+++ ProyectoAguaPruebaUnitarias/UsuarioBLTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoAgua.BL;
+using ProyectoAgua.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAgua.BL.Tests
+{
+    [TestClass()]
+    public class UsuarioBLTests
+    {
+        private Usuario usuarioInicial = new Usuario { Id = 4, IdRol = 2, Login = "RGonzales", Password = "123456" };
+        private UsuarioBL usuarioBL = new UsuarioBL();
+        [TestMethod()]
+        public async Task CrearAsyncTest()
+        {
+            Usuario usuario = new Usuario();
+            usuario.IdRol = usuarioInicial.IdRol;
+            usuario.Nombre = "Robertito";
+            usuario.Apellido = "Gonzalez";
+            usuario.Login = "RGonzales";
+            usuario.Password = "123456";
+            usuario.Estatus = (byte)Estatus_Usuario.ACTIVO;
+            int result = await usuarioBL.CrearAsync(usuario);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod()]
+        public async Task T2ModificarAsyncTest()
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = usuarioInicial.Id;
+            usuario.IdRol = usuarioInicial.IdRol;
+            usuario.Nombre = "Roberto";
+            usuario.Apellido = "Gonzales";
+            usuario.Login = "RGonzales";
+            usuario.Password = "123456";
+            usuario.Estatus = (byte)Estatus_Usuario.INACTIVO;
+            var result = await usuarioBL.ModificarAsync(usuario);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod()]
+        public async void DeleteAsyncTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public async Task T3ObtenerPorIdAsyncTest()
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = usuarioInicial.Id;
+            var result = await usuarioBL.ObtenerPorIdAsync(usuario);
+            Assert.IsTrue(result.Id == usuario.Id);
+        }
+
+        [TestMethod()]
+        public async Task T4ObtenerTodosAsyncTest()
+        {
+            var resul = await usuarioBL.ObtenerTodosAsync();
+            Assert.IsTrue(resul.Count > 0);
+        }
+
+        [TestMethod()]
+        public async Task BuscarAsyncTest()
+        {
+            Usuario usuario = new Usuario();
+            usuario.IdRol = usuarioInicial.IdRol;
+            usuario.Nombre = "a";
+            usuario.Apellido = "a";
+            usuario.Login = "a";
+            usuario.Estatus = (byte)Estatus_Usuario.ACTIVO;
+            usuario.Top_Aux = 10;
+            var resul = await usuarioBL.BuscarAsync(usuario);
+            Assert.AreNotEqual(0, resul.Count);
+        }
+
+        [TestMethod()]
+        public async Task BuscarIncluirRolAsyncTest()
+        {
+            Usuario usuario = new Usuario();
+            usuario.IdRol = usuarioInicial.IdRol;
+            usuario.Nombre = "a";
+            usuario.Apellido = "ViDuran";
+            usuario.Login = "a";
+            usuario.Estatus = (byte)Estatus_Usuario.ACTIVO;
+            usuario.Top_Aux = 10;
+            var resulUsuarios = await usuarioBL.BuscarIncluirRolAsync(usuario);
+            var ultimoUsuario = resulUsuarios.FirstOrDefault();
+            Assert.IsTrue(ultimoUsuario.Rol != null && usuario.IdRol == ultimoUsuario.Rol.Id);
+           
+        }
+
+        [TestMethod()]
+        public async Task LoginAsyncTest()
+        {
+
+            Usuario usuario = new Usuario();
+            usuario.Apellido = "ViDuran";
+            usuario.Password = "123456";
+            var resul = await usuarioBL.LoginAsync(usuario);
+            Assert.AreEqual(usuario.Login,"ViDuran");
+        }
+
+        [TestMethod()]
+        public void CambiarPasswordAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
